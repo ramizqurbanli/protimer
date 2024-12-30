ae66d6a62c362ec3502537072532acd9f44e1b89
Ramiz Qurbanli <ramiz@qurbanli.com>
2024-12-30 15:19:21 +0400

    ksksksk

--- settings_save.py
+++ settings_save.py
@@ -1,14 +1,21 @@
 import json
 
-
 def save_settings(settings):
-        with open('settings.json', 'w') as f:
-            json.dump(settings, f, indent=4)
+    if settings is None:
+        settings = {}
+    with open('settings.json', 'w') as f:
+        json.dump(settings, f, indent=4)
 
 def load_settings():
-        try:
-            with open('settings.json', 'r') as f:
-                return json.load(f)
-        except :
-            save_settings(None)
-            return False
+    try:
+        with open('settings.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        save_settings({})
+        return {}
+    except json.JSONDecodeError:
+        print("Error decoding JSON. Using default settings.")
+        return {}
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return {}
