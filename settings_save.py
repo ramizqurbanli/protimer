3165ccc578502f955eca770151b71f4c0e3e1ebf
Ramiz Qurbanli <ramiz@qurbanli.com>
2024-12-30 15:27:11 +0400

    sjsjsjsj

--- settings_save.py
+++ settings_save.py
@@ -1,14 +1,20 @@
 import json
+import os
+
+def get_settings_path():
+    app_data_dir = os.path.join(os.getenv('APPDATA'), 'ProTimer')
+    os.makedirs(app_data_dir, exist_ok=True)
+    return os.path.join(app_data_dir, 'settings.json')
 
 def save_settings(settings):
     if settings is None:
         settings = {}
-    with open('settings.json', 'w') as f:
+    with open(get_settings_path(), 'w') as f:
         json.dump(settings, f, indent=4)
 
 def load_settings():
     try:
-        with open('settings.json', 'r') as f:
+        with open(get_settings_path(), 'r') as f:
             return json.load(f)
     except FileNotFoundError:
         save_settings({})
